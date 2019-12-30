492ee18f0f12f556a8348a8bccfd850449fe7acc
AmirAli Mirian <amirali.mirian@gmail.com>
2019-12-30 10:34:50 +0330

    added filp flops

--- src/modules.sv
+++ src/modules.sv
@@ -7,3 +7,23 @@ module MUX2 #(parameter WIDTH = 8)
         out = s ? s1: s2;
 
 endmodule
+
+module FlipFlop #(parameter WIDTH = 8)
+                      (input logic[WIDTH-1:0] d,
+                       input logic clk,
+                       output logic[WIDTH-1:0] out);
+    
+    always_ff @(posedge clk)
+        out <= d;
+
+endmodule
+
+module FlipFlopEn #(parameter WIDTH = 8)
+                      (input logic[WIDTH-1:0] d,
+                       input logic clk, en,
+                       output logic[WIDTH-1:0] out);
+    
+    always_ff @(posedge clk)
+        if (en) out <= d;
+
+endmodule
