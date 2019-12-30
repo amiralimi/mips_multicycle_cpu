0bf469e59d56bce26482866b2e6d735515d81e0b
AmirAli Mirian <amirali.mirian@gmail.com>
2019-12-30 20:26:33 +0330

    added a control unit module

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -98,3 +98,12 @@ module ALUDecoder(input logic[5:0] funct,
             alu_control = 3'b110;
 
 endmodule
+
+module ControlUnit(input logic[5:0] opcode, funct, 
+                   output logic mem_to_reg, reg_dest, i_or_d, alu_src_a, ir_write, mem_write, pc_write, branch, reg_write,
+                   output logic[1:0] alu_src_b, pc_src,
+                   output logic[2:0] alu_control);
+    logic [1:0] alu_op; 
+    StateMachine sm(clk, reset, opcode, mem_to_reg, reg_dest, i_or_d, alu_src_a, ir_write, mem_write, pc_write, branch, reg_write, alu_src_b, pc_src, alu_op);
+    ALUDecoder ad(funct, alu_op, alu_control);
+endmodule
