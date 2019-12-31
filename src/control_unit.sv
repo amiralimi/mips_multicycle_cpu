a296298c3ec8d5e85b680087b0ee9e51810d5120
AmirAli Mirian <amirali.mirian@gmail.com>
2019-12-31 19:18:59 +0330

    added clk to control unit inputs

--- src/control_unit.sv
+++ src/control_unit.sv
@@ -1,5 +1,5 @@
 module ControlUnit(input logic[5:0] opcode, funct, 
-                  output logic mem_to_reg, reg_dest, i_or_d, alu_src_a, ir_write, mem_write, pc_write, branch, reg_write,
+                  output logic clk, mem_to_reg, reg_dest, i_or_d, alu_src_a, ir_write, mem_write, pc_write, branch, reg_write,
                   output logic[1:0] alu_src_b, pc_src,
                   output logic[2:0] alu_control);
    
